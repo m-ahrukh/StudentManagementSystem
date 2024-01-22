9f6eb68bc29db48d07d42ebc59a9202450674d91
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-01-22 16:23:12 +0500

    Get All Students Api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server"
+import { pool } from "../utils/dbConnect"
 
 export async function GET() {
+    const data = await pool.query("SELECT * FROM student")
+    const result = data.rows
     return NextResponse.json({
-        hello: "world"
+        result
     })
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,47 @@
-import Image from "next/image";
+import { GET } from "./api/route";
 import dbConnect from "./utils/dbConnect";
 
-export default function Home() {
+type Student = {
+  stu_id?: number,
+  stu_name: string,
+  stu_age: number,
+  graduated: boolean
+}
+export default async function Home() {
 
   dbConnect()
+
+  const response = await GET()
+  const responseData = await response.json();
+  if (!responseData) {
+    return null;
+  }
+  const result = responseData.result || [];
+
   return (
-    <div>
-      Student Management System
+    <div className="flex flex-col gap-5">
+      <h1>Student Management System</h1>
+      <table className="border shadow-md">
+        <thead className="border shadow-md">
+          <tr>
+            <th className="border px-5">Student Id</th>
+            <th className="border px-5">Student Name</th>
+            <th className="border px-5">Student Age</th>
+            <th className="border px-5">Graduated</th>
+          </tr>
+        </thead>
+        <tbody>
+          {result.map((dataRow: Student, index: number) => (
+            <tr key={index}>
+              <td className="border text-center">{index+1}</td>
+              <td className="border text-center">{dataRow.stu_name}</td>
+              <td className="border text-center">{dataRow.stu_age}</td>
+              <td className="border text-center">{dataRow.graduated? 'true': 'false'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button className="bg-blue-400 p-3 rounded w-32">Add Student</button>
     </div>
   );
 }
